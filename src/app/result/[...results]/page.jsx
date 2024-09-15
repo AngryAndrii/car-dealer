1d18ac06143846f74fa69f797a0affa7c53fb743
Andrii Chychkan <mywork1728@gmail>
2024-09-15 18:35:32 +0300

    try to fix

--- src/app/result/[...results]/page.jsx
+++ src/app/result/[...results]/page.jsx
@@ -1,43 +1,37 @@
+// 'use client';
 import { fetchCars } from "@/app/helpers/fetch";
+// import { useState } from 'react';
 
 export async function generateStaticParams() {
 
   const cars = await fetchCars();
   const carsResult = cars.Results;
   const years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
-  return carsResult.map((car) => ({
-    id: car.MakeId,
-   }))
+  let ids = []
+  ids = carsResult.map((car) => (ids.push(car.MakeId)))
+  let paramsList = []
+  for (let id of ids) {
+    for (let year in years) {
+      paramsList.push({ id: id.toString(), year: year.toString() });
+    }
+  }
+  return paramsList;
 }
 
-console.log(generateStaticParams())
-
-export default function Page({ params }) {
-  // console.log(params);
+export default function Page({ paramsList }) {
+  // const [cars, setCars] = useState([]);
+  console.log(paramsList);
 
   const getCars = async () => {
-    const cars = await fetchCarModelAndId();
+    const cars = await fetchCarModelAndId(params[0], params[1]);
     const carsResult = cars.Results;
     setCars(carsResult);
+    console.log(cars);
   };
+
+  useEffect(() => {
+    getCars();
+  }, []);
 
   return <>Result page with params</>;
 }
-
-
-
-
-
-// // Generate segments for both [category] and [product]
-// export async function generateStaticParams() {
-//   const products = await fetch('https://.../products').then((res) => res.json())
- 
-//   return products.map((product) => ({
-//     category: product.category.slug,
-//     product: product.id,
-//   }))
-// }
- 
-// export default function Page({ params }) {
-//   // ...
-// }
