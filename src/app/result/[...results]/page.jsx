53005fa1b77ace76dc05173e20439e9df5b22949
Andrii Chychkan <mywork1728@gmail>
2024-09-15 17:16:26 +0300

    added generate static params functions

--- src/app/result/[...results]/page.jsx
+++ src/app/result/[...results]/page.jsx
@@ -4,15 +4,23 @@ export async function generateStaticParams() {
 
   const cars = await fetchCars();
   const carsResult = cars.Results;
+  const years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
   return carsResult.map((car) => ({
     id: car.MakeId,
-    year: car.Year,
-  }))
-  return [];
+   }))
 }
 
+console.log(generateStaticParams())
+
 export default function Page({ params }) {
-  console.log(params);
+  // console.log(params);
+
+  const getCars = async () => {
+    const cars = await fetchCarModelAndId();
+    const carsResult = cars.Results;
+    setCars(carsResult);
+  };
+
   return <>Result page with params</>;
 }
 
@@ -20,16 +28,16 @@ export default function Page({ params }) {
 
 
 
-// Generate segments for both [category] and [product]
-export async function generateStaticParams() {
-  const products = await fetch('https://.../products').then((res) => res.json())
+// // Generate segments for both [category] and [product]
+// export async function generateStaticParams() {
+//   const products = await fetch('https://.../products').then((res) => res.json())
  
-  return products.map((product) => ({
-    category: product.category.slug,
-    product: product.id,
-  }))
-}
+//   return products.map((product) => ({
+//     category: product.category.slug,
+//     product: product.id,
+//   }))
+// }
  
-export default function Page({ params }) {
-  // ...
-}
+// export default function Page({ params }) {
+//   // ...
+// }
